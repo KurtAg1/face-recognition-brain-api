55d11270a973995fad020c5005d93cc9adae0cd4
KurtAg1 <kurtagius613@gamil.com>
2020-06-09 16:19:59 +0200

    safer api key

--- controllers/image.js
+++ controllers/image.js
@@ -2,7 +2,7 @@ const Clarifai = require('clarifai');
 
 //You must add your own API key here from Clarifai.
 const app = new Clarifai.App({
-    apiKey: '64d76f06fca6439486cf20196f08ed50'
+    apiKey: process.env.API_CLARIFAI
 });
 
 const handleApiCall = (req, res) => {
